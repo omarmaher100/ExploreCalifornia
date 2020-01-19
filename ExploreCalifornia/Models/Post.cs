015214054cefce5be606444dc9c225f2579f4112
Omar Maher <omarmaher100@gmail.com>
2020-01-19 14:00:46 +0300

    Validations

--- ExploreCalifornia/Models/Post.cs
+++ ExploreCalifornia/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        [Display(Name = "Post Title")]
+        [Required]
+        [StringLength(150 ,MinimumLength = 15 , ErrorMessage = "Title must be more thans 15 and less than 150")]
         public string Title { get; set; }
         public string Author { get; set; }
+        [Required]
+        [MinLength(110 ,ErrorMessage ="Post must be at least 110 char")]
         public string Body { get; set; }
         public DateTime Posted { get; set; }
 
