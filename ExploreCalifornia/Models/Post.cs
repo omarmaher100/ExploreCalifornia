83e2981f97fa54ce2e3798f9022ecaed5982a71f
Omar Maher <omarmaher100@gmail.com>
2020-01-19 14:39:42 +0300

    Helper

--- ExploreCalifornia/Models/Post.cs
+++ ExploreCalifornia/Models/Post.cs
@@ -13,9 +13,12 @@ namespace ExploreCalifornia.Models
         [StringLength(150 ,MinimumLength = 15 , ErrorMessage = "Title must be more thans 15 and less than 150")]
         public string Title { get; set; }
         public string Author { get; set; }
+
         [Required]
         [MinLength(110 ,ErrorMessage ="Post must be at least 110 char")]
+        [DataType(DataType.MultilineText)]
         public string Body { get; set; }
+
         public DateTime Posted { get; set; }
 
     }
