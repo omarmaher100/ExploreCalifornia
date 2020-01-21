198309705a556daa854ef8fb7be3ecc02eb09794
Omar Maher <omarmaher100@gmail.com>
2020-01-21 10:25:07 +0300

    ViewComponent

---
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.ViewComponents
+{
+   // [ViewComponent]
+    public class MonthlySpecialsViewComponent
+    {
+        public string Invoke()
+        {
+            return "I'm the view component";
+        }
+    }
+}
