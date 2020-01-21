933e4459c0166b3a15521cad3273475aeefb80c9
Omar Maher <omarmaher100@gmail.com>
2020-01-21 11:14:44 +0300

    adding ViewComponent

---
+++ ExploreCalifornia/Models/MonthlySpecial.cs
@@ -0,0 +1,10 @@
+﻿namespace ExploreCalifornia.Models
+{
+    public class MonthlySpecial
+    {
+        public string Key { get; internal set; }
+        public string Name { get; internal set; }
+        public string Type { get; internal set; }
+        public int Price { get; internal set; }
+    }
+}
--- ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
+++ ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -1,17 +1,24 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExploreCalifornia.ViewComponents
 {
-   // [ViewComponent]
-    public class MonthlySpecialsViewComponent
+  //  [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
     {
-        public string Invoke()
+        private readonly BlogDataContext _dp;
+        public MonthlySpecialsViewComponent(BlogDataContext dp)
         {
-            return "I'm the view component";
+            this._dp = dp;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var specials = _dp.MonthlySpecials.ToArray();
+            return View(specials);
         }
     }
 }
